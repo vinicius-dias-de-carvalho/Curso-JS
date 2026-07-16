cdc74aabfe4a7d1e4cc37071a7279b63127c7e96
vinicius-dias <vinicius20811@gmail.com>
2026-07-16 15:03:45 -0300

    funções

---
+++ aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parimpar(n){
+    if(n%2 == 0){
+     return 'Par!'
+    }else{
+        return 'Impar!'
+    }
+        
+}
+
+console.log(parimpar(223))
+
+//let res = parimpar(223)
+//console.log(res)
