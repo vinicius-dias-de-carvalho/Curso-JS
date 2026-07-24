19c9379b863f9d09f8be53f7d008c543422c3cfd
vinicius-dias <vinicius20811@gmail.com>
2026-07-24 16:02:25 -0300

    ex reforço

---
+++ aula16ex/script.js
@@ -0,0 +1,36 @@
+let num = document.getElementById('fnum') 
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n){
+   if(Number(n) >= 1 && Number(n) <= 100){
+    return true
+
+}else{
+    return false
+   }
+
+} 
+
+function inLista(n, l) {
+   if (l.indexOf(Number(n)) != -1 ){
+    return true
+   
+ }else{
+    
+    return false
+  
+}
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        
+    }else{
+        window.alert('Valor invalido ou já encontrado na lista. ')
+    }
+    
+    
+}
+   
---
+++ ex revisão/modelo/script.js
@@ -0,0 +1,4 @@
+function mudar(){
+let rm = document.getElementById('titulo')
+rm.innerText = (`Você me clicou!`) 
+}
