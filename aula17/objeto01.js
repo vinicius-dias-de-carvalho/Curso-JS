0e414977148d404e1f5f68d2fec2793b880c5978
vinicius-dias <vinicius20811@gmail.com>
2026-07-30 09:01:27 -0300

    ex09

---
+++ aula17/objeto01.js
@@ -0,0 +1,13 @@
+let amigo = {nome:'jose',
+sexo:'m',
+peso: 85.4,
+engordar(p){
+  console.log('Engordou')
+  this.peso += p
+}
+
+}
+
+amigo.engordar(2)
+
+console.log(`${amigo.nome} pesa ${amigo.peso}kg`)
